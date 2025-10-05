b802e3223cd280f71c9396dcb97c1b24016930b7
KimothoMuchiri <muchirikimotho16@gmail.com>
2025-10-05 19:47:40 +0300

    implemented views and tested with post man

--- alx_travel_app/listings/views.py
+++ alx_travel_app/listings/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Listing, Bookings
+from .serializers import ListingSerializer, BookingSerializer
+
+class ListingViewSet(viewsets.ModelViewSet):
+    # queryset tells the mdel which objects it should work with
+    queryset = Listing.objects.all()
+
+    # serializer tells the viewset how to convert model data
+    serializer_class = ListingSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    # queryset tells the mdel which objects it should work with
+    queryset = Bookings.objects.all()
+
+    # serializer tells the viewset how to convert model data
+    serializer_class = BookingSerializer
 
 # Create your views here.
